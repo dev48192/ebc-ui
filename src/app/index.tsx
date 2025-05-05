70ba8903350f9eed5b6fa0fb42fabb99fb3954a8
Buddhadeb Das <40224680+dev48192@users.noreply.github.com>
2025-05-05 05:56:18 +0000

    rspack integration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,8 +4,6 @@ import Styles from './Styles.module.scss';
 // component
 
 const App: React.FC = () => {
-  return (
-    <div className={Styles['container']}>Hello EBc user</div>
-  );
-}
+  return <div className={Styles['container']}>Hello EBc user</div>;
+};
 export default App;
