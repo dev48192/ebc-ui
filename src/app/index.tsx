897c36199ae06a3a6273cc31abf249b498d5b913
Sudipta Ghosh <sidipta2001@gmail.com>
2025-05-05 21:06:27 +0530

    add mui

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Styles from './Styles.module.scss';
 import Users from '../components/Users';
+import Button from '@mui/material/Button';
 
 // component
 
 const App: React.FC = () => {
   return <div className={Styles['container']}>
     <h1>Hello EBc user</h1>
+    <Button variant="contained">Hello world</Button>
+
     <Users/>
   </div>;
 };
