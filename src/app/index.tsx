973bd33c350b3b602673e2017052f3f185305b57
Sudipta Ghosh <sidipta2001@gmail.com>
2025-05-08 12:46:42 +0530

    Page-layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -14,13 +14,13 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import HomeIcon from "@mui/icons-material/Home";
+import CalculateIcon from "@mui/icons-material/Calculate";
+import GroupsIcon from "@mui/icons-material/Groups";
+import SettingsIcon from "@mui/icons-material/Settings";
 import CardContainer from "../components/CardContainer";
 import AppBar from "../components/AppBar";
-import { ViewSidebar } from "@mui/icons-material";
 import SideBar from "../components/SideBar";
-
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
@@ -43,148 +43,150 @@ export default function App() {
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={[
-              {
-                marginRight: 5,
-              },
-              open && { display: "none" },
-            ]}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <SideBar variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                        justifyContent: "initial",
-                      }
-                    : {
-                        justifyContent: "center",
-                      },
-                ]}
-              >
-                <ListItemIcon
+    <>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={[
+                {
+                  marginRight: 5,
+                },
+                open && { display: "none" },
+              ]}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              EBC
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <SideBar variant="permanent" open={open}>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {["Home", "Cost Calculator"].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                <ListItemButton
                   sx={[
                     {
-                      minWidth: 0,
-                      justifyContent: "center",
+                      minHeight: 48,
+                      px: 2.5,
+                      py: 1.5,
                     },
                     open
                       ? {
-                          mr: 3,
+                          justifyContent: "initial",
                         }
                       : {
-                          mr: "auto",
+                          justifyContent: "center",
                         },
                   ]}
                 >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1,
-                        }
-                      : {
-                          opacity: 0,
-                        },
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                        justifyContent: "initial",
-                      }
-                    : {
+                  <ListItemIcon
+                    sx={[
+                      {
+                        minWidth: 0,
                         justifyContent: "center",
                       },
-                ]}
-              >
-                <ListItemIcon
+                      open
+                        ? {
+                            mr: 3,
+                          }
+                        : {
+                            mr: "auto",
+                          },
+                    ]}
+                  >
+                    {index % 4 === 0 ? <HomeIcon /> : <CalculateIcon />}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={text}
+                    sx={[
+                      open
+                        ? {
+                            opacity: 1,
+                          }
+                        : {
+                            opacity: 0,
+                          },
+                    ]}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+
+            {["Conference hall", "Setting"].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                <ListItemButton
                   sx={[
                     {
-                      minWidth: 0,
-                      justifyContent: "center",
+                      minHeight: 48,
+                      px: 2.5,
+                      py: 1,
                     },
                     open
                       ? {
-                          mr: 3,
+                          justifyContent: "initial",
                         }
                       : {
-                          mr: "auto",
+                          justifyContent: "center",
                         },
                   ]}
                 >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1,
-                        }
-                      : {
-                          opacity: 0,
-                        },
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </SideBar>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <CardContainer />
+                  <ListItemIcon
+                    sx={[
+                      {
+                        minWidth: 0,
+                        justifyContent: "center",
+                      },
+                      open
+                        ? {
+                            mr: 3,
+                          }
+                        : {
+                            mr: "auto",
+                          },
+                    ]}
+                  >
+                    {index % 4 === 0 ? <GroupsIcon /> : <SettingsIcon />}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={text}
+                    sx={[
+                      open
+                        ? {
+                            opacity: 1,
+                          }
+                        : {
+                            opacity: 0,
+                          },
+                    ]}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </SideBar>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <CardContainer />
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
--- src/components/CardContainer/index.tsx
+++ src/components/CardContainer/index.tsx
@@ -1,17 +1,170 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import { red, blue, green } from "@mui/material/colors";
+import { red, blue, green, pink, purple, yellow } from "@mui/material/colors";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 
 const CardContainer: React.FC = () => {
   return (
     <>
-      <Card sx={{ maxWidth: 355 }}>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Paper>
+            <Card sx={{ maxWidth: 355 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    MD
+                  </Avatar>
+                }
+                title="Material Depo"
+                subheader="September 14, 2016"
+              />
+
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  This impressive paella is a perfect party dish and a fun meal
+                  to cook together with your guests. Add 1 cup of frozen peas
+                  along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing></CardActions>
+            </Card>
+          </Paper>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Paper>
+            {" "}
+            <Card sx={{ maxWidth: 355 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: blue[500] }} aria-label="recipe">
+                    TD
+                  </Avatar>
+                }
+                title="Technical Desk"
+                subheader="September 14, 2016"
+              />
+
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  This impressive paella is a perfect party dish and a fun meal
+                  to cook together with your guests. Add 1 cup of frozen peas
+                  along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing></CardActions>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Paper>
+            {" "}
+            <Card sx={{ maxWidth: 355 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
+                    FA
+                  </Avatar>
+                }
+                title="Fabricator Area"
+                subheader="September 14, 2016"
+              />
+
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  This impressive paella is a perfect party dish and a fun meal
+                  to cook together with your guests. Add 1 cup of frozen peas
+                  along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing></CardActions>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Paper>
+            {" "}
+            <Card sx={{ maxWidth: 355 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: yellow[900] }} aria-label="recipe">
+                    MH
+                  </Avatar>
+                }
+                title="Manpower Hub"
+                subheader="September 14, 2016"
+              />
+
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  This impressive paella is a perfect party dish and a fun meal
+                  to cook together with your guests. Add 1 cup of frozen peas
+                  along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing></CardActions>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Paper>
+            {" "}
+            <Card sx={{ maxWidth: 355 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: purple[500] }} aria-label="recipe">
+                    HT
+                  </Avatar>
+                }
+                title="Hiring Terminal"
+                subheader="September 14, 2016"
+              />
+
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  This impressive paella is a perfect party dish and a fun meal
+                  to cook together with your guests. Add 1 cup of frozen peas
+                  along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing></CardActions>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Paper>
+            {" "}
+            <Card sx={{ maxWidth: 355 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: pink[500] }} aria-label="recipe">
+                    CZ
+                  </Avatar>
+                }
+                title="Contract Zon"
+                subheader="September 14, 2016"
+              />
+
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  This impressive paella is a perfect party dish and a fun meal
+                  to cook together with your guests. Add 1 cup of frozen peas
+                  along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing></CardActions>
+            </Card>
+          </Paper>
+        </Grid>
+      </Grid>
+      {/* <Card sx={{ maxWidth: 355 }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -30,11 +183,11 @@ const CardContainer: React.FC = () => {
           </Typography>
         </CardContent>
         <CardActions disableSpacing></CardActions>
-      </Card>
+      </Card> */}
 
       {/* card2 */}
 
-      <Card sx={{ maxWidth: 355 }}>
+      {/* <Card sx={{ maxWidth: 355 }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: blue[500] }} aria-label="recipe">
@@ -52,31 +205,30 @@ const CardContainer: React.FC = () => {
             the mussels, if you like.
           </Typography>
         </CardContent>
+        <CardActions disableSpacing></CardActions> */}
+
+      {/* card 3 */}
+
+      {/* <Card sx={{ maxWidth: 355 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
+              FA
+            </Avatar>
+          }
+          title="Fabricator Area"
+          subheader="September 14, 2016"
+        />
+
+        <CardContent>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
         <CardActions disableSpacing></CardActions>
-
-        {/* card 3 */}
-
-        <Card sx={{ maxWidth: 355 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
-                FA
-              </Avatar>
-            }
-            title="Fabricator Area"
-            subheader="September 14, 2016"
-          />
-
-          <CardContent>
-            <Typography variant="body2" sx={{ color: "text.secondary" }}>
-              This impressive paella is a perfect party dish and a fun meal to
-              cook together with your guests. Add 1 cup of frozen peas along
-              with the mussels, if you like.
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing></CardActions>
-        </Card>
-      </Card>
+      </Card> */}
     </>
   );
 };
